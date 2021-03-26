f89019dbfb6353aaa2c3be8462b26f5ed86ab7de
Eshaan <eshaanmandal29@gmail.com>
2021-03-26 21:25:19 +0530

    Toggles between 8 LED using timers instead of loops

---
+++ led_array_timers/main.c
@@ -0,0 +1,41 @@
+#include <msp430.h> 
+#include<inttypes.h>
+volatile unsigned int i;
+volatile char led=0;
+void register_settings_GPIO()
+{
+    P1DIR |= 0xFF;
+    P1OUT |= 0x00;
+}
+void register_settings_timer()
+{
+    TACCTL0 = CCIE;
+    TACTL = MC_1 + TASSEL_1;
+    TACCR0 = 32768;
+}
+void setup_ACLK()
+{
+    do
+    {
+        IFG1 &= ~OFIFG;
+        __delay_cycles(50000);
+    }
+    while(IFG1 & OFIFG);
+}
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	register_settings_GPIO();
+	setup_ACLK();
+	register_settings_timer();
+	__bis_SR_register(GIE + LPM3_bits);
+	while(1)
+	    ;
+}
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void leds(void)
+{
+    P1OUT = led++;
+    if(led > 7)
+        led =0;
+}
