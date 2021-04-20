1494edf4f330e79db048a1ef42b0c33eb8620c45
Eshaan <eshaanmandal29@gmail.com>
2021-04-20 10:59:17 +0530

    Introduction to UART based serial communication

---
+++ Serial_Communication_MSP430/main.c
@@ -0,0 +1,44 @@
+#include <msp430.h>
+
+/**
+ * @brief
+ * These settings are wrt enabling uart on Lunchbox
+ **/
+void register_settings_for_UART()
+{
+    P1SEL = BIT1 + BIT2;              // Select UART RX/TX function on P1.1,P1.2
+    P1SEL2 = BIT1 + BIT2;
+
+    UCA0CTL1 |= UCSSEL_1;               // UART Clock -> ACLK
+    UCA0BR0 = 3;                        // Baud Rate Setting for 32kHz 9600
+    UCA0BR1 = 0;                        // Baud Rate Setting for 32kHz 9600
+    UCA0MCTL = UCBRF_0 + UCBRS_3;       // Modulation Setting for 32kHz 9600
+    UCA0CTL1 &= ~UCSWRST;               // Initialize UART Module
+    IE2 |= UCA0RXIE;                    // Enable RX interrupt
+}
+
+/*@brief entry point for the code*/
+void main(void)
+{
+    WDTCTL = WDTPW + WDTHOLD;           //! Stop Watchdog (Not recommended for code in production and devices working in field)
+
+    register_settings_for_UART();
+
+    __bis_SR_register(LPM0_bits + GIE); // Enter LPM0, Enable Interrupt
+
+}
+
+/**
+ * @brief
+ * Interrupt Vector for UART on LunchBox
+ **/
+#pragma vector=USCIAB0RX_VECTOR         // UART RX Interrupt Vector
+__interrupt void USCI0RX_ISR(void)
+{
+    while (!(IFG2 & UCA0TXIFG));          // Check if TX is ongoing
+    if(UCA0RXBUF >= 97 && UCA0RXBUF <= 122)
+            UCA0TXBUF = UCA0RXBUF + 3;
+    else
+        UCA0TXBUF = UCA0RXBUF + 1;
+
+}
