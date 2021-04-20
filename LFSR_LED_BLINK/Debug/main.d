645f5cedc31702690a6a4153229702f891da3ba4
Eshaan <eshaanmandal29@gmail.com>
2021-04-20 10:58:28 +0530

    Using LFSR to randomly blink LED

---
+++ LFSR_LED_BLINK/Debug/main.d
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/inttypes.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/linkage.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_inttypes.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdio.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdarg.h
+
+../main.c: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/inttypes.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_inttypes.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdio.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdarg.h: 
